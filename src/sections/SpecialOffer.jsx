86a43dfba4e522518687908b5fb771158359cd02
Bilegt Davaa <bilegt.davaa@lcieducation.net>
2023-12-13 00:21:36 -0500

    Finished the Nike Single Page App

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,8 +1,40 @@
-
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "../components/Button"
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer} width={773} height={687} className="object-contain w-full"/>
+      </div>
+
+      <div className="flex flex-1 flex-col">
+        <h2 className="text-4xl font-palanquin font-bold">
+          <span className="text-coral-red">
+            Special </span>
+          Offer
+        </h2>
+
+        <p className="mt-4 info-text">
+          Get the latest drip no kizzy ehehehe
+        </p>
+
+        <p className="mt-5 info-text">
+          The drippiest of them all
+        </p>
+
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label='Shop Now' iconURL={arrowRight}/>
+          <Button label='Learn More' backgroundColor='white'
+            borderColor='border-slate-gray'
+            textColor='text-slate-gray'
+          />
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,11 +1,12 @@
-import Button from "../components/Button"
+import Button from "../components/Button";
+import shoe8 from "../assets/images/shoe8.svg";
 
 const SuperQuality = () => {
   return (
     <section id="about-us" className="flex justify-between items-center max-lg:flex-col
     gap-10 w-full max-container">
       <div className="flex flex-1 flex-col">
-            <h1 className='mt-10 font-palanquin text-8xl
+            <h2 className='mt-10 font-palanquin text-8xl
             max-sm:text-[72px] max-sm:leading-[82]'>
                 <span className='xl:bg-white
                 xl:whitespace-nowrap relative z-10 pr-10'>
@@ -17,14 +18,28 @@ const SuperQuality = () => {
                     Nike
                 </span>
                 Shoes
-            </h1>
-            <p className='font-montserrat
-            text-slate-gray text-lg leading-8 mt-6 mb-10
-            sm:max-w-sm
+            </h2>
+            <p className='mt-4 lg:max-w-lg info-text
             '>Drippy Nike Shoes
             </p>
-            <Button label="View More"/>
+
+            <div className="mt-11">
+              <Button label="View More"/>
+            </div>    
       </div>
+
+      <div className="flex-1 flex justify-center items-center">
+
+        <img
+          src={shoe8}
+          alt='product detail'
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+
+      </div>
+
     </section>
   )
 }
